4688f547d8b1ef4eef6a7828910cd47d5d3f4ef7
moroneyishwari05 <ishwarimoroney595@gmail.com>
2026-01-04 22:38:22 +0530

    Add binary search method to Solution class
    This implementation provides a method to perform binary search on a sorted array, returning the index of the target element if found.

---
+++ BinarySearch.java
@@ -0,0 +1,33 @@
+//It has a Runtime complexity of O(log n)
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while ( low <= high){
+            int mid = low + (high-low)/2;
+       
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            else if(target < nums[mid]){
+                 high = mid - 1;
+            }
+
+            else {
+                 low = mid + 1;
+            }
+        
+        } 
+        
+        return -1;
+        
+    }
+
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int nums[] = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(sol.search(nums, target));
+    }
+}
